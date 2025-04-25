bb0f748d21b21b30312ced677878937c50c7ee4e
Amal Kariyawasam <12756149+1Amal@users.noreply.github.com>
2025-04-25 16:35:28 +1000

    Add Index.jsx component

---
+++ frontend/src/components/Index.jsx
@@ -0,0 +1,10 @@
+function Index() {
+  return (
+    <div>
+      <h1>Where's Maverick (A photo tagging Game)</h1>
+      <button>Start Game</button>
+    </div>
+  );
+}
+
+export default Index;
--- frontend/src/components/main.jsx
+++ frontend/src/components/main.jsx
@@ -1,10 +1,13 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import '../assets/index.css'
-import App from '../components/App'
+// import App from '../components/App'
+
+import Index from '../components/Index.jsx'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <App />
+    {/* <App /> */}
+    <Index/>
   </StrictMode>,
 )
