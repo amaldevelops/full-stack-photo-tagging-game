9db19d1e173c8fc8e5ea2dd7d706af34fc6c45b8
Amal Kariyawasam <12756149+1Amal@users.noreply.github.com>
2025-05-16 10:28:30 +1000

    Fixed CORS issue and tested with fetching data

--- frontend/src/components/LeaderBoard.jsx
+++ frontend/src/components/LeaderBoard.jsx
@@ -1,6 +1,26 @@
 import React, { useEffect, useState } from "react";
 
+const apiURL = import.meta.env.VITE_API_URL;
+
 function LeaderBoard() {
+  useEffect(() => {
+    async function fetchLeaderBoard() {
+      try {
+        let response = await fetch(`${apiURL}/leaderboard`);
+
+        if (!response.ok) {
+          throw new Error("HTTP Error! Status", response.status);
+        }
+
+        const queryResult = await response.json();
+        console.log(queryResult);
+      } catch (err) {
+        console.error(err);
+      }
+    }
+    fetchLeaderBoard();
+  }, []);
+
   return (
     <div>
       <h1>Leader Board</h1>
